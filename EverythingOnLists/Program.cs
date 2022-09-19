3c2c5facc255afd3d766594d2899fdc329c2a43f
elijah amoako <elijahamoako72@gmail.com>
2022-09-19 04:56:25 +0000

    first commit

---
+++ EverythingOnLists/Program.cs
@@ -0,0 +1,15 @@
+﻿var productList = new List<string>
+{
+    "Product 001",
+    "Product 002",
+    "Product 003",
+    "Product 004"
+};
+
+for (int i = 0; i < productList.Count; i++)
+{
+    var productName = productList[i];
+    Console.WriteLine(productName);
+}
+
+Console.ReadLine();
