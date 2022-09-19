01ba5da9fb147497a71b7a976b94e0694c71e2e9
elijah amoako <elijahamoako72@gmail.com>
2022-09-19 05:55:15 +0000

    added more

--- EverythingOnLists/Program.cs
+++ EverythingOnLists/Program.cs
@@ -6,10 +6,36 @@
     "Product 004"
 };
 
+
+/* using the add method*/
+productList.Add("Product 005");
+
+var newProductList = new List<string>
+{
+    "Product A",
+    "Product B",
+    "Product C"
+};
+/*using addrange method*/
+productList.AddRange(newProductList);
+
+/*using insert method*/
+productList.Insert(2, "Product Inserted");
+
+/*using remove method*/
+productList.Remove("Product 001");
+
+
+
 for (int i = 0; i < productList.Count; i++)
 {
     var productName = productList[i];
     Console.WriteLine(productName);
 }
+
+
+
+
+
 
 Console.ReadLine();
