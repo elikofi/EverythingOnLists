8ee3e3559630d120cea0616ada3ddc4a580e5dd5
elijah amoako <elijahamoako72@gmail.com>
2022-09-20 05:45:05 +0000

    updated

---
+++ EverythingOnLists/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EverythingOnLists
+{
+    public class Product
+    {
+        public string  ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public decimal Price { get; set; }
+
+
+
+        public Product()
+        {
+        }
+    }
+}
+
--- EverythingOnLists/Program.cs
+++ EverythingOnLists/Program.cs
@@ -1,41 +1,91 @@
-﻿var productList = new List<string>
+﻿using EverythingOnLists;
+
+
+//var productList = new List<string>
+//{
+//    "Product 001",
+//    "Product 002",
+//    "Product 003",
+//    "Product 004"
+//};
+
+
+//productList.Add("Product 005");
+
+//var newProductList = new List<string>
+//{
+//    "Product A",
+//    "Product B",
+//    "Product C"
+//};
+//productList.AddRange(newProductList);
+
+//productList.Insert(2, "Product Inserted");
+
+//productList.Remove("Product 001");
+
+//var result = productList.Contains("Product 003");
+////Console.WriteLine(result);
+
+
+
+
+
+
+
+//for (int i = 0; i < productList.Count; i++)
+//{
+//    var productName = productList[i];
+//    Console.WriteLine(productName);
+//}
+
+
+/*  USING THE CLASS PRODUCT    */
+
+
+var productList = new List<Product>
 {
-    "Product 001",
-    "Product 002",
-    "Product 003",
-    "Product 004"
+    new Product
+    {
+        ProductCode = "101",
+        ProductName = "Product 101",
+        Price = 100
+    },
+    new Product
+    {
+        ProductCode = "102",
+        ProductName = "Product 102",
+        Price = 200
+    },
+    new Product
+    {
+        ProductCode = "103",
+        ProductName = "Product 103",
+        Price = 300
+    }
+
 };
+//using the exists method
+var result = productList.Exists(x => x.Price > 300);
+Console.WriteLine(result); //will return false.
+//using the find method
+var pFind = productList.FindAll(x => x.Price > 100);
+Console.WriteLine(pFind);
 
-
-/* using the add method*/
-productList.Add("Product 005");
-
-var newProductList = new List<string>
+/*adding a new product   */
+productList.Add(new Product
 {
-    "Product A",
-    "Product B",
-    "Product C"
-};
-/*using addrange method*/
-productList.AddRange(newProductList);
+    ProductCode = "104",
+    ProductName = "Product 104",
+    Price = 400
+});
+//removing some products
+productList.RemoveAt(2);
 
-/*using insert method*/
-productList.Insert(2, "Product Inserted");
-
-/*using remove method*/
-productList.Remove("Product 001");
-
-
-
-for (int i = 0; i < productList.Count; i++)
+foreach (var product in productList)
 {
-    var productName = productList[i];
-    Console.WriteLine(productName);
+    Console.WriteLine($"Name: {product.ProductName} | Code: {product.ProductCode} | Price: {product.Price}");
 }
-
-
-
-
 
 
 Console.ReadLine();
